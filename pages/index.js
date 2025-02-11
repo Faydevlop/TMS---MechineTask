aff1ed25b592bc807952279f2f0cf1f25747320f
fayis <fayfayis74@gmail.com>
2025-02-11 12:56:24 +0530

    build success - 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-
-
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  return (
-   <div>
-    hello
-   </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/auth/login"); // Redirect to login
+  }, []);
+
+  return null; // Prevent rendering content before redirect
 }
